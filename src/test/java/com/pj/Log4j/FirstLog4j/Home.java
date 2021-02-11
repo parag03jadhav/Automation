96dccdc67fc6afc8b1500389b349fa852c62e020
parag03jadhav <parag03jadhav@gmail.com>
2021-02-11 11:14:03 +0530

    New msg

---
+++ src/test/java/com/pj/Log4j/FirstLog4j/Home.java
@@ -0,0 +1,12 @@
+package com.pj.Log4j.FirstLog4j;
+
+public class Home {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("parag");
+	}
+
+}
